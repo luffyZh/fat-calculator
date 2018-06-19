7047c32b19c490b4340a066dafb9d507323e00bb
luffyZhou <zhoudeyou945@126.com>
2018-06-19 22:09:25 +0800

    version 1.0.1

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -2,17 +2,21 @@ import Taro, { Component } from '@tarojs/taro';
 import { View, Text, Button } from '@tarojs/components';
 import Result from '../../components/result/result';
 import './index.scss';
+import shareImage from './shareImage.png';
+
 
 export default class Index extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      heightValue: 0,
-      weightValue: 0,
+      heightValue: '',
+      weightValue: '',
       errorShow: 'none',
       resultShow: 'none',
       finalResult: 0,
+      avatarUrl: '',
+      nickName: ''
     };
   }
 
@@ -22,13 +26,26 @@ export default class Index extends Component {
 
   componentWillMount () { }
 
-  componentDidMount () { }
+  componentDidMount () {}
 
   componentWillUnmount () { }
 
   componentDidShow () { }
 
   componentDidHide () { }
+
+  // 设置分享
+  onShareAppMessage = (res) => {
+    if (res.from === 'button') {
+      // 来自页面内转发按钮
+      console.log(res.target)
+    }
+    return {
+      title: '您有一份体质健康数据，请查收！',
+      path: '/pages/index/index'
+    }
+  }
+
 
   heightValueChange = (e) => {
     const heightValue = e.target.value;
@@ -41,39 +58,76 @@ export default class Index extends Component {
   }
 
   calculatorFat = () => {
-    const { heightValue, weightValue } = this.state;
-    
-    if (heightValue !== 0 && weightValue !== 0) {
-      if (Number(heightValue) < 0 || Number(heightValue) > 2.5) {
-        Taro.showModal({
-          title: '身高确认？',
-          content: '身高单位为米，请确认您的身高数据正确！',
-          success: (res) => {
-            if (res.confirm) {
+    wx.getUserInfo({
+      success: (res) => {
+        const { userInfo } = res;
+        const { avatarUrl, nickName } = userInfo;
+        this.setState({ avatarUrl, nickName }, () => {
+          const { heightValue, weightValue } = this.state;
+          if (heightValue !== 0 && weightValue !== 0) {
+            if (Number(heightValue) < 0 || Number(heightValue) > 2.5) {
+              Taro.showModal({
+                title: '身高确认？',
+                content: '身高单位为米，请确认您的身高数据正确！',
+                success: (res) => {
+                  if (res.confirm) {
+                    // 身高正确，进行计算
+                    const finalResult = Number(weightValue) / (Number(heightValue) * Number(heightValue));
+                    this.setState({ resultShow: 'block', finalResult: finalResult.toFixed(1) });
+                  } else if (res.cancel) {
+                    console.log('用户点击取消')
+                  }
+                }
+              })
+            } else {
               // 身高正确，进行计算
               const finalResult = Number(weightValue) / (Number(heightValue) * Number(heightValue));
-              this.setState({ resultShow: 'block', finalResult });
-            } else if (res.cancel) {
-              console.log('用户点击取消')
+              this.setState({ resultShow: 'block', finalResult: finalResult.toFixed(1) });
             }
+          } else {
+            this.setState({ errorShow: 'flex' }, () => {
+              setTimeout(() => {
+                this.setState({ errorShow: 'none' });
+              }, 1800);
+            })
           }
-        })
-      } else {
-        // 身高正确，进行计算
-        const finalResult = Number(weightValue) / (Number(heightValue) * Number(heightValue));
-        this.setState({ resultShow: 'block', finalResult: finalResult.toFixed(1) });
+        });
+      },
+      fail: (res) => {
+        const { heightValue, weightValue } = this.state;
+        if (heightValue !== 0 && weightValue !== 0) {
+          if (Number(heightValue) < 0 || Number(heightValue) > 2.5) {
+            Taro.showModal({
+              title: '身高确认？',
+              content: '身高单位为米，请确认您的身高数据正确！',
+              success: (res) => {
+                if (res.confirm) {
+                  // 身高正确，进行计算
+                  const finalResult = Number(weightValue) / (Number(heightValue) * Number(heightValue));
+                  this.setState({ resultShow: 'block', finalResult: finalResult.toFixed(1) });
+                } else if (res.cancel) {
+                  console.log('用户点击取消')
+                }
+              }
+            })
+          } else {
+            // 身高正确，进行计算
+            const finalResult = Number(weightValue) / (Number(heightValue) * Number(heightValue));
+            this.setState({ resultShow: 'block', finalResult: finalResult.toFixed(1) });
+          }
+        } else {
+          this.setState({ errorShow: 'flex' }, () => {
+            setTimeout(() => {
+              this.setState({ errorShow: 'none' });
+            }, 1800);
+          })
+        }
       }
-    } else {
-      this.setState({ errorShow: 'flex' }, () => {
-        setTimeout(() => {
-          this.setState({ errorShow: 'none' });
-        }, 1800);
-      })
-    }
+    })
   }
 
   hideResult = () => {
-    this.setState({ resultShow: 'none' });
+    this.setState({ resultShow: 'none', weightValue: '', heightValue: '' });
   }
 
   render () {
@@ -87,6 +141,8 @@ export default class Index extends Component {
           hideResult={this.hideResult}
           resultShow={this.state.resultShow}
           finalResult={this.state.finalResult}
+          avatarUrl={this.state.avatarUrl}
+          nickName={this.state.nickName}
         />
         <div className='index-bg'>
           <div className='errorHint' style={errorStyle}>
@@ -96,12 +152,12 @@ export default class Index extends Component {
           <View className='index'>
             <div className='index-card'>
               <Text className='defination'>
-                体重指数：目前常用的体重指数（body mass index）简称BMI，又译为体脂指数。
+                体重指数：目前常用的体重指数（body mass index）简称BMI，又译为体质指数。
               </Text>
               <Text className='pattern'>
-                BMI = 身高 / 体重²
+                BMI = 体重(kg) ÷ 身高²(m)
               </Text>
-              <Text className='attention'>** : 男性腰围 > 85, 女性腰围 > 60 为腹型肥胖！</Text>
+              <Text className='attention'>男性腰围 ≥ 85cm, 女性腰围 ≥ 80cm 为腹型肥胖！</Text>
               <Text className='originArticle'>《中国成人超重及肥胖指南》</Text>
             </div>
             <div className='index-card'>
@@ -110,6 +166,7 @@ export default class Index extends Component {
                 <Input
                   className='form-input'
                   type='digit'
+                  value={this.state.heightValue}
                   placeholder='请输入您的身高'
                   placeholderClass='form-placeholder'
                   onChange={this.heightValueChange}
@@ -121,6 +178,7 @@ export default class Index extends Component {
                 <Input
                   className='form-input'
                   type='digit'
+                  value={this.state.weightValue}
                   placeholder='请输入您的体重'
                   placeholderClass='form-placeholder'
                   onChange={this.weightValueChange}
@@ -138,6 +196,7 @@ export default class Index extends Component {
                   type='primary'
                   plain
                   onClick={this.calculatorFat}
+                  openType='getUserInfo'
                 >计算</Button>
               </View>
             </div>
